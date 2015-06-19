f5ecec95cdfb32a0cef264ae8648d4d05c09d2a0
williamdeng <williamdeng@williamdeng-PC0.tencent.com>
2015-06-19 16:55:50 +0800

    as

--- FileTrans/src/com/example/filetrans/MainActivity.java
+++ FileTrans/src/com/example/filetrans/MainActivity.java
@@ -7,7 +7,14 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -47,7 +54,7 @@ public class MainActivity extends Activity {
 				String urlStr = "http://www.whatsact.com:10087/json";
 				String urlStr2 = "http://pica.nipic.com/2007-11-09/2007119124513598_2.jpg";
 				try {
-					HttpSender.post(urlStr, jo, new TextResponse(), HttpRequestType.POST_DATA);
+					HttpSender.get(urlStr2, new ShowResponse(), HttpRequestType.POST_DATA);
 				} catch (MalformedURLException e) {
 					e.printStackTrace();
 				}
@@ -96,8 +103,7 @@ public class MainActivity extends Activity {
 
 		@Override
 		public void onResponse(Object respContent) {
-			//tv.setText(respContent.toString());
-			imView.setImageBitmap((Bitmap)respContent);
+			imView.setImageBitmap(getRoundedCornerBitmap((Bitmap)respContent));
 		}
 	}
 	
@@ -120,5 +126,39 @@ public class MainActivity extends Activity {
 			
 		}
 	}
+	
+	
+	public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) 
+	{
+		Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Config.ARGB_8888);
+		Canvas canvas = new Canvas(output);
+		final Paint paint = new Paint();
+		int width = bitmap.getWidth();
+		int height = bitmap.getHeight();
+		int w;
+		int deltaX = 0;
+		int deltaY = 0;
+
+		if (width <= height) {
+			w = width;
+			deltaY = height - w;
+		} 
+		else {
+			w = height;
+			deltaX = width - w;
+
+		}
+		final Rect rect = new Rect(deltaX, deltaY, w, w);
+		final RectF rectF = new RectF(rect);
+		paint.setAntiAlias(true);
+		canvas.drawARGB(0, 0, 0, 0);  
+		int radius = (int) (Math.sqrt(w * w * 2.0d) / 2);
+	    canvas.drawRoundRect(rectF, radius, radius, paint);
+	    paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+	    canvas.drawBitmap(bitmap, rect, rect, paint);
+
+	    return output;
+	}
+
 	
 }
